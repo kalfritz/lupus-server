f7f9e28c5a4d4fc552a8a978442f12bb191d318d
Luiz Elias <luizeliasceem@gmail.com>
2020-01-09 19:14:03 -0300

    fix/typo

--- src/app/models/Comment.js
+++ src/app/models/Comment.js
@@ -15,7 +15,7 @@ class Comment extends Model {
   static associate(models) {
     this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
     this.belongsTo(models.Post, { foreignKey: 'post_id', as: 'post' });
-    this.belongsToMany(models.user, {
+    this.belongsToMany(models.User, {
       foreignKey: 'user_id',
       through: 'comment_likes',
       as: 'likes',
