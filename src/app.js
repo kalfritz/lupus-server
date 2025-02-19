34ab5098250016585ff77b69712c8f58c21d4b51
Luiz Ramos <kalfritz@Luizs-MacBook-Pro.local>
2025-02-19 01:40:19 -0300

    cors cors cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 
 import helmet from 'helmet';
+import cors from 'cors';
 import redis from 'redis';
 import RateLimit from 'express-rate-limit';
 import RateLimitRedis from 'rate-limit-redis';
@@ -21,13 +22,7 @@ class App {
   constructor() {
     this.app = express();
     this.server = http.Server(this.app);
-    this.io = socketio(this.server, {
-      cors: {
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-        credentials: true,
-      },
-    });
+    this.io = socketio(this.server);
 
     this.middlewares();
     this.connections();
@@ -37,6 +32,15 @@ class App {
 
   middlewares() {
     this.app.use(helmet());
+    this.app.use(
+      cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+      })
+    );
+
     this.app.use(express.json());
     this.app.use(
       '/files',
